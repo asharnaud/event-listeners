d85db003006062418137a6f3a7ec1ca729a25429
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-04-04 15:33:56 -0500

    Finished the bonus exercise

--- main.js
+++ main.js
@@ -13,19 +13,34 @@ function hideNavMenu () {
 
 document.querySelector('.nav-btn').addEventListener('click', hideNavMenu)
 
-
 function addGuestToList (key) {
-  var text = document.querySelector('.user-input').value
   var liEl = document.createElement('li')
   var list = document.querySelector('#add-guest .guest-list')
   liEl.classList.add('guest')
-  if (key.keyCode === 13) {
-    if (this.value !== '') {
-      liEl.textContent = this.value
-      list.appendChild(liEl)
-      this.value = ''
-    }
+  if (key.keyCode === 13 && this.value !== '') {
+    liEl.textContent = this.value
+    list.appendChild(liEl)
+    this.value = ''
   }
 }
 
 document.querySelector('.answer-box input').addEventListener('keypress', addGuestToList)
+
+function addGuestWithBtn (key) {
+  if (key.keyCode === 13 && this.value !== '') {
+    var btnEl = document.createElement('button')
+    btnEl.textContent = 'X'
+    btnEl.addEventListener('click', function () {
+      this.parentElement.remove()
+    })
+    var liEl = document.createElement('li')
+    liEl.classList.add('guest')
+    liEl.textContent = this.value
+    liEl.appendChild(btnEl)
+    var list = document.querySelector('#bonus-list .guest-list')
+    list.appendChild(liEl)
+    this.value = ''
+  }
+}
+
+document.querySelector('#bonus-list input').addEventListener('keypress', addGuestWithBtn)
